6d9fd0d333ccb9205339cf3002cc448e6ce0c43a
Jürgen Löhel <2422012+jloehel@users.noreply.github.com>
2025-05-13 16:11:18 -0600

    Support a list of XML elements for custom helpers
    First, really simple approach
    
    Signed-off-by: Jürgen Löhel <2422012+jloehel@users.noreply.github.com>

--- py_serializable/__init__.py
+++ py_serializable/__init__.py
@@ -511,6 +511,9 @@ class _XmlSerializable(Protocol):
                             pass  # skip the element
                         elif isinstance(v_ser, Element):
                             this_e.append(v_ser)
+                        elif isinstance(v_ser, list[Element]):
+                            for element in v_ser:
+                                this_e.append(v_ser)
                         else:
                             SubElement(this_e, new_key).text = _xs_string_mod_apply(str(v_ser),
                                                                                     prop_info.xml_string_config)
